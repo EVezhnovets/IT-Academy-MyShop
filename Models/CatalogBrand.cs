bc1c9ff6481558969719f882ed508f30e93968a9
Euheni <eugene.vezhnavets@gmail>
2022-12-05 21:51:34 +0300

    Add catalog comntroller

--- Models/CatalogBrand.cs
+++ Models/CatalogBrand.cs
@@ -4,7 +4,7 @@
     {
         //TODO replace GUID
         public int Id { get; set; }
-
+        public string Brand{ get; set; }
 
     }
 }
--- Models/CatalogItemViewModel.cs
+++ Models/CatalogItemViewModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id{ get; set; }
         public string Name { get; set; }
-        public string PicctureUrl { get; set; }
+        public string PictureUrl { get; set; }
         public decimal Price { get; set; }
     }
 }
