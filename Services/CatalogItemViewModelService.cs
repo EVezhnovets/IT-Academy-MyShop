c0ac0770ca8fe0f65d03f515d567e6d3e57061a4
Euheni <eugene.vezhnavets@gmail>
2022-12-07 21:58:53 +0300

    Add bl

---
+++ Services/CatalogItemViewModelService.cs
@@ -0,0 +1,13 @@
+﻿using MyShop.Interfaces;
+using MyShop.Models;
+
+namespace MyShop.Services
+{
+    public class CatalogItemViewModelService : ICatalogItemViewModelService
+    {
+        void ICatalogItemViewModelService.UpdateCatalogItem(CatalogItemViewModel catalogItemViewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
