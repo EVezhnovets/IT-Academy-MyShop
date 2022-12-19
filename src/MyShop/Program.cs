bb288863e9444ce58603d03f867aab20d10ab459
Euheni <eugene.vezhnavets@gmail>
2022-12-19 19:29:33 +0300

    Fixing structure

---
+++ src/MyShop/Program.cs
@@ -0,0 +1,38 @@
+using MyShop.Configuration;
+using MyShop.Interfaces;
+using MyShop.Models;
+using MyShop.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddCoreServices();
+builder.Services.AddScoped(typeof(IRepository<CatalogItem>), typeof(LocalCatalogItemRepository));
+builder.Services.AddScoped<ICatalogItemViewModelService, CatalogItemViewModelService>();
+
+var app = builder.Build();
+app.Logger.LogInformation("App created...");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Logger.LogDebug("Starting the app... ");
+app.Run();
---
+++ src/MyShop/Services/CatalogItemViewModelService.cs
@@ -0,0 +1,41 @@
+﻿using MyShop.ApplicationCore.Interfaces;
+using MyShop.Interfaces;
+using MyShop.Models;
+
+namespace MyShop.Services
+{
+    public class CatalogItemViewModelService : ICatalogItemViewModelService
+    {
+        private readonly IRepository<CatalogItem> _catalogItemRepository;
+        private readonly IAppLogger<CatalogItemViewModelService> _logger;
+
+        public CatalogItemViewModelService(IRepository<CatalogItem> catalogItemRepository,
+            IAppLogger<CatalogItemViewModelService> logger)
+        {
+            _catalogItemRepository = catalogItemRepository;
+            _logger = logger;
+        }
+        public void UpdateCatalogItem(CatalogItemViewModel viewModel)
+        {
+            var existingCatalogItem = _catalogItemRepository.GetById(viewModel.Id);
+
+            if (existingCatalogItem is null)
+            {
+                var exception = new Exception($"Catalog item {viewModel.Id} not found");
+                _logger.LogError(exception, exception.Message);
+
+                throw exception;
+            }
+
+            CatalogItem.CatalogItemDetails detail = new(viewModel.Name, viewModel.Price);
+            existingCatalogItem.UpdateDetails(detail);
+
+            _logger.LogInformation($"Updating catalog item {existingCatalogItem.Id}. " +
+                $"Name {existingCatalogItem.Name}. " +
+                $"Price {existingCatalogItem.Price}");
+
+            _catalogItemRepository.Update(existingCatalogItem);
+
+        }
+    }
+}
